dc83cea06da635c68c11abec54c91391d799cfd3
dsabalete <dsabalete@gmail.com>
2016-05-26 18:27:27 +0000

    Stars rating and sorting in the Meteor app

--- meteor-development/my_first_app/image_share.js
+++ meteor-development/my_first_app/image_share.js
@@ -3,7 +3,9 @@ console.log(Images.find().count());
 
 if (Meteor.isClient) {
    
-   Template.images.helpers({images:Images.find()});
+   Template.images.helpers({images:
+      Images.find({}, {sort: {rating: -1}})
+   });
    
    Template.images.events({
     'click .js-image':function(event){
@@ -12,11 +14,21 @@ if (Meteor.isClient) {
     'click .js-del-image':function(event) {
        var image_id = this._id;
        console.log(image_id);
+       // use jquery to hide the image component
+       // then remove it at the end of the animation
        $("#" + image_id).hide('slow', function() {
            Images.remove({"_id":image_id});
        });
+    },
+    'click .js-rate-image': function(event) {
+      var rating = $(event.currentTarget).data("userrating");
+      console.log(rating);
+      var image_id = this.id;
+      console.log(image_id);
       
-    }
+      Images.update({_id: image_id}, 
+                    {$set: {rating:rating}});
+    },
    });
 
 }
