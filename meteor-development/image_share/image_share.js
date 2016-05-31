0ac238af7b59830d804dd56acf2d6d1849fd8653
dsabalete <dsabalete@gmail.com>
2016-05-31 21:02:47 +0000

    Infinite scroll - Meteor Dev

--- meteor-development/image_share/image_share.js
+++ meteor-development/image_share/image_share.js
@@ -3,6 +3,27 @@ console.log(Images.find().count());
 
 if (Meteor.isClient) {
     
+    Session.set("imageLimit", 8);
+    
+    lastScrollTop = 0;
+    $(window).scroll(function(event) {
+        // test if we are near the bottom of window
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+           // where are we in the page?
+            var scrollTop = $(this).scrollTop();
+            // see if we are going up or down
+            if (scrollTop > lastScrollTop) {
+                console.log("down");
+                Session.set("imageLimit", Session.get("imageLimit") + 4);
+            } else {
+                console.log("up");
+            }
+            
+            lastScrollTop = scrollTop;
+        }
+    });
+    
+    
     Accounts.ui.config({
         passwordSignupFields: "USERNAME_AND_EMAIL"
     });
@@ -12,7 +33,7 @@ if (Meteor.isClient) {
             if (Session.get("userFilter")) { // they set a filter!
                 return Images.find({createdBy:Session.get("userFilter")}, {sort: {createdOn: -1, rating: -1}});
             } else {
-                return Images.find({}, {sort: {createdOn: -1, rating: -1}});
+                return Images.find({}, {sort: {createdOn: -1, rating: -1}, limit:Session.get("imageLimit")});
             }
             
         },
