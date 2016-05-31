3e7f22c013626b68a04cce2d7eef6b766526c77b
dsabalete <dsabalete@gmail.com>
2016-05-31 09:58:29 +0000

    Accessing user information - Meteor Dev

--- meteor-development/image_share/image_share.js
+++ meteor-development/image_share/image_share.js
@@ -3,38 +3,47 @@ console.log(Images.find().count());
 
 if (Meteor.isClient) {
    
-   Template.images.helpers({images:
-      Images.find({}, {sort: {createdOn: -1, rating: -1}})
-   });
+    Template.images.helpers({images:
+        Images.find({}, {sort: {createdOn: -1, rating: -1}})
+    });
    
-   Template.images.events({
-    'click .js-image':function(event){
-        $(event.target).css("width", "50px");
-    },
-    'click .js-del-image':function(event) {
-       var image_id = this._id;
-       console.log(image_id);
-       // use jquery to hide the image component
-       // then remove it at the end of the animation
-       $("#" + image_id).hide('slow', function() {
-           Images.remove({"_id":image_id});
-       });
-    },
-    'click .js-rate-image': function(event) {
-      var rating = $(event.currentTarget).data("userrating");
-      console.log(rating);
-      var image_id = this.id;
-      console.log(image_id);
-      
-      Images.update({_id: image_id}, 
-                    {$set: {rating:rating}});
-    },
-    'click .js-show-image-form': function(event) {
-        console.log('js-show-image-form');
-        $("#image_add_form").modal('show');
-    },
-   }); // end of image Template
+    Template.body.helpers({username: function() {
+        if (Meteor.user()) {
+            return Meteor.user().emails[0].address;
+        } else {
+            return "anonymous internet user";
+        }
+    }
+    });
    
+   
+    Template.images.events({
+        'click .js-image':function(event){
+            $(event.target).css("width", "50px");
+        },
+        'click .js-del-image':function(event) {
+            var image_id = this._id;
+            console.log(image_id);
+            // use jquery to hide the image component
+            // then remove it at the end of the animation
+            $("#" + image_id).hide('slow', function() {
+                Images.remove({"_id":image_id});
+            });
+        },
+        'click .js-rate-image': function(event) {
+            var rating = $(event.currentTarget).data("userrating");
+            console.log(rating);
+            var image_id = this.id;
+            console.log(image_id);
+  
+            Images.update({_id: image_id}, 
+                {$set: {rating:rating}});
+        },
+        'click .js-show-image-form': function(event) {
+            console.log('js-show-image-form');
+            $("#image_add_form").modal('show');
+        },
+    }); // end of image Template
    
     Template.image_add_form.events({
         'submit .js-add-image': function(event) {
